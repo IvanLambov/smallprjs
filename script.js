3f35a97e872c07a763b4126e66b6213af1ad86a5
Ivan <hui@gmail.com>
2021-05-22 10:34:37 +0200

    zapisi v massiv i function

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let numberOfFilms = +prompt('How many films did you see ?', '');
+"use strict"
+let numberOfFilms;
 
+function start() {
+    numberOfFilms = +prompt('How many films did you see ?', '');
+    //proverkstroki
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films did you see ?', '');
+    }
+}
+start();
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -10,43 +19,70 @@ let personalMovieDB = {
 
 
 //const a = prompt('One of the last seen films?', ''),
-  //  b = prompt('The mark of that film?', ''),
-   // c = prompt('One of the last seen films?', ''),
-   // d = prompt('The mark of that film?', '');
+//  b = prompt('The mark of that film?', ''),
+// c = prompt('One of the last seen films?', ''),
+// d = prompt('The mark of that film?', '');
 //personalMovieDB.movies[a] = b;
 //personalMovieDB.movies[c] = d;
 //console.log(personalMovieDB);
 
 //1 avtomatizirovali ziklom
-for(let i = 0; i<2; i++){
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+
+        const a = prompt('One of the last seen films?', ''),
+            b = prompt('The mark of that film?', '');
+
+        //null eto cancel. polizovateli ne najal knopku cancel  a.lenght - kol-vo simvolov 
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+
+        } else {
+            console.log('Error');
+            //inace vozvrahaemsea na 1 hag nazad 
+            i--;
+        }
+
+
+    }
+}
+///rememberMyFilms();
+
+
+function detectPersonaLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('You have seen not a lot films');
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        console.log('klassik men ');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('You are tv-MAn');
+    } else {
+        console.log('erroe');
+    }
+}
+//detectPersonaLevel();
+//vivodim bazu esli ona publi4naia v konsoli
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres() {
+    for (let r = 1; r <= 3; r++) {
+        let genre = prompt(`Your favorite genres ${r}`);
+        //zapisivaem dannie v massiv 
+        personalMovieDB.genres[r -1] = genre ;
+
+    }
     
-   const a = prompt('One of the last seen films?', ''),
-    b = prompt('The mark of that film?', '');
-
-//null eto cancel. polizovateli ne najal knopku cancel  a.lenght - kol-vo simvolov 
- if ( a!= null && b !=null && a!= '' && b != '' && a.length<50){
-personalMovieDB.movies[a] = b;
-console.log('done');
-
- }else{
-     console.log('Error');
-     //inace vozvrahaemsea na 1 hag nazad 
-     i--;
- }
-
-
+    
 }
-
-
-if (personalMovieDB.count<10){
-console.log('You have seen not a lot films');
-}else if(personalMovieDB.count >10 && personalMovieDB.count <30){
-console.log('klassik men ');
-}else if (personalMovieDB.count>=30){
-    console.log('You are tv-MAn');
-}else{
-    console.log('erroe');
-}
+writeYourGenres();
 
 console.log(personalMovieDB);
 
